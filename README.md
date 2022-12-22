15105c8097f92f182e755030d2f957b083f60a26
Eric Davis <ericscottdavis@outlook.com>
2022-12-22 13:34:26 -0500

    Scripts to call loops with SIP.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # YY1P
 
 Analyses for the YY1P project.
+
+## Calling loops with SIP
+
+The following scripts were run on UNC longleaf to call loops
+from bio-rep combined Hi-C files with SIP:
+
+```{bash}
+sh callLoopsBioreps.sh
+sh callLoopsGenotype.sh
+```
---
+++ scripts/processing/callLoopsGenotype.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+hicFiles=$(ls /proj/phanstiel_lab/Data/processed/YY1P/hic/novaseq/hg38/221214_dietJuicerMerge/output/{KO,WT}/*inter_30.hic)
+sip_jar="/proj/phanstiel_lab/Software/SIP/SIP_HiCv1.6.2.jar"
+chromSizes="/proj/phanstiel_lab/References/chromSizes/hg38_chromSizes.txt"
+juicer_tools_jar="/proj/phanstiel_lab/Software/juicer/scripts/juicer_tools_1.14.08.jar"
+
+for f in $hicFiles
+do
+ name=$(basename $f .hic)
+ echo $name
+ jid=`sbatch <<- SIP | egrep -o -e "\b[0-9]+$"
+	#!/bin/sh
+	#SBATCH -J $name
+	#SBATCH -p general
+	#SBATCH -n 1
+	#SBATCH -N 1
+	#SBATCH --mem=8G
+	#SBATCH -t 4320
+	#SBATCH -o %x_%j.out
+	#SBATCH -e %x_%j.err
+
+	java -jar $sip_jar \
+	hic $f \
+	$chromSizes \
+	$name \
+	$juicer_tools_jar \
+	-g 2.0 -t 2000 -fdr 0.05
+
+SIP`
+	echo Submitted jobid: $jid
+done
